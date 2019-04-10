3e089acb41880e85c51a1818d3486f51a445f698
DungKHoang <DungKHoang@users.noreply.github.com>
2019-04-10 16:11:00 -0500

    Update PreCheck-Upgrade.ps1
    Prompt dfor admin account-
    Add Save backup and folder for backup and Dump
    Simplify code

--- PreCheck-Upgrade.ps1
+++ PreCheck-Upgrade.ps1
@@ -1,9 +1,19 @@
 Param ($credential , $hostname)
 
+# Create folder to host backup and Dump
+$currentFolder  = Split-Path $MyInvocation.MyCommand.Path
+$folder         = "$currentFolder\OV-Backup-Dump"
+
+if (-not (test-path $folder))
+{
+  md $folder
+}
+
+
 # Check connection
 if (-not ($credential))
 {
-    $credential = get-credential -message "Please provide admin credential to log into...."
+    $credential = get-credential -message "Please provide **** admin credential***  to log to OneView...."
 }
 
 if (-not ($hostname))
@@ -18,7 +28,7 @@ Connect-HPOVMgmt -hostname $hostname -Credential $cred
 
 
 write-host -foreground CYAN '1#  save the Appliance support dump to C:'
-New-HPOVSupportDump -location  C: -type appliance
+New-HPOVSupportDump -location  $folder -type appliance
 
 
 write-host -foreground CYAN  '2# generate a new backup, and if a remote location is configured, will initiate the file transfer. If not configured, then the Cmdlet will download the file to the current working directory.'
@@ -27,17 +37,15 @@ New-HPOVBackup
 write-host -foreground CYAN 'The backup file is..'
 Get-HPOVBackup
 
+write-host -foreground CYAN 'Downloading the backup file..'
+Save-HPOVBackup -location $folder
 
 write-host
 write-host '3# check composer health active/standy cluster'
-$Composers = Get-HPOVComposerNode
-Foreach ($Composer in $Composers) {
-  write-host $Composer.ApplianceConnection,$composer.modelNumber,$composer.Name,$composer.Role,$composer.state,$composer.status
-}
+Get-HPOVComposerNode | select ApplianceConnection,modelNumber,Name,Role,state,status
 
 write-host -foreground CYAN '4# check appliance web server certificate expiration'
-$AppCert = Get-HPOVApplianceCertificateStatus
-write-host $AppCert.ApplianceConnection   $AppCert.validUntil
+Get-HPOVApplianceCertificateStatus | select  ApplianceConnection,validUntil
 
 write-host -foreground CYAN '5# Confirm no critical alerts on the uplinks or logical Enclosure or interconnect modules'
 'Enclosure','InterconnectBay','Network'| % { get-hpovalert -Timespan (New-TimeSpan -Days 2) -HealthCategory $_   | where Severity -eq 'Critical' }
